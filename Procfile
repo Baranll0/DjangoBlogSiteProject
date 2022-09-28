dc920a8cef53690c4e70d8eb0b6cb72381a85a16
gbaran180@gmail.com <gbaran180@gmail.com>
2022-09-28 14:51:22 +0300

    First-projects

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn Site.wsgi:apllication --log-file -
--- Site/settings.py
+++ Site/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os.path
 from pathlib import Path
+import django_heroku
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+#@y^+!o)3=el0u=r8ixp-!4$fdt(k)nzljtj&vhssz9jeo7vi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -36,6 +39,7 @@ INSTALLED_APPS = [
     "django_cleanup",
     "simple_chatbot",
     "Contact",
+
 
 ]
 
@@ -131,6 +135,7 @@ CKEDITOR_CONFIGS={
 }
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
