7ec1c2f3a9042e014b8c7b8c9da49644147fa741
gbaran180@gmail.com <gbaran180@gmail.com>
2022-10-28 09:09:32 +0300

    Debug True False

--- Site/settings.py
+++ Site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+#@y^+!o)3=el0u=r8ixp-!4$fdt(k)nzljtj&vhssz9jeo7vi'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 ALLOWED_HOSTS = ['https://django-projectyazilim.herokuapp.com']
 # Application definition
 INSTALLED_APPS = [
