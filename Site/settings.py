84ee7f987f43548bac31a0fd7fc8a593772b3fbf
gbaran180@gmail.com <gbaran180@gmail.com>
2023-05-13 19:27:08 +0300

    commit

--- Site/settings.py
+++ Site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+#@y^+!o)3=el0u=r8ixp-!4$fdt(k)nzljtj&vhssz9jeo7vi'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =True
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 # Application definition
 INSTALLED_APPS = [
     'django.contrib.admin',
