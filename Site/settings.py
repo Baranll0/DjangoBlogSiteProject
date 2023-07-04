61a98ee93c1503a2edd455cbd23f438ccdb9bd76
gbaran180@gmail.com <gbaran180@gmail.com>
2023-07-05 00:58:18 +0300

    Update csrf

--- Site/settings.py
+++ Site/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CSRF_COOKIE_SECURE = True
 
 ROOT_URLCONF = 'Site.urls'
 TEMPLATES = [ 
