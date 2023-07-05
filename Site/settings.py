e10f7bfcc32c7b37621218df9a2f61cdedcc6a0b
gbaran180@gmail.com <gbaran180@gmail.com>
2023-07-05 12:25:18 +0300

    update

--- Site/settings.py
+++ Site/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+#@y^+!o)3=el0u=r8ixp-!4$fdt(k)nzljtj&vhssz9jeo7vi'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =False
+DEBUG =True
 ALLOWED_HOSTS = ['www.baranguclu.site', 'baranguclu.site','localhost','https://baran-guclu-910e9820ce43.herokuapp.com']
 # Application definition
 INSTALLED_APPS = [
